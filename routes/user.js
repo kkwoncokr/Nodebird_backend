a6c2a7449839d95056c8c05fccfd1f8511858d4a
kkwoncokr <kkwoncokr@gmail.com>
2020-11-12 16:35:05 +0900

    [ADD] [ADD] 개인별 user 정보 추가.

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport')
-const { User,Post } = require('../models') // 구조분해 할당
+const { User,Post,Image,Comment } = require('../models') // 구조분해 할당
 const router = express.Router();
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
@@ -155,7 +155,40 @@ router.patch('/nickname', isLoggedIn, async (req,res,next) => {
     next(err)
   }
 })
-
+router.get('/:userId', async (req,res,next)=> {
+  try {
+    const fullUserWithoutPassword = await User.findOne({
+      where : {id:req.params.userId},
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model:Post,
+        attributes: ['id']
+      },{
+        model: User,
+        as:'Followings',
+        attributes:['id']
+      },{
+        model:User,
+        as:'Followers',
+        attributes:['id']
+      }]
+    })
+    if(fullUserWithoutPassword) {
+      const data = fullUserWithoutPassword.toJSON();
+      data.Posts = data.Posts.length;
+      data.Followings = data.Followings.length;
+      data.Followers = data.Followers.length;
+      res.status(200).json(data);
+    }else {
+      res.status(400).json('존재하지 않는 사용자입니다.')
+    }
+  }catch(err) {
+    console.error(err);
+    next(err);
+  }
+})
 // 팔로우
 router.patch('/:userId/follow', isLoggedIn, async (req,res,next) => {
   try {
@@ -230,5 +263,48 @@ router.delete('/follower/:userId', isLoggedIn, async (req,res,next) => {
   }
 })
 
+router.get('/:userId/posts', async (req, res, next) => { // GET /user/1/posts
+  try {
+    const where = { UserId: req.params.userId };
+    if (parseInt(req.query.lastId, 10)) { // 초기 로딩이 아닐 때
+      where.id = { [Op.lt]: parseInt(req.query.lastId, 10)}
+    } // 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [['createdAt', 'DESC']],
+      include: [{
+        model: User,
+        attributes: ['id', 'nickname'],
+      }, {
+        model: Image,
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+          order: [['createdAt', 'DESC']],
+        }],
+      }, {
+        model: User, // 좋아요 누른 사람
+        as: 'Likers',
+        attributes: ['id'],
+      }, {
+        model: Post,
+        as: 'Retweet',
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }, {
+          model: Image,
+        }]
+      }],
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 module.exports  = router;
